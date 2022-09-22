12ec9d692b2171829b540b5fcbfb08adf0ad8e10
HuyThanh <huythanh1308bn@gmail.com>
2022-09-22 17:23:55 +0700

    update 22/09

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,32 @@ import reportWebVitals from './reportWebVitals'
 import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import Router from './router'
+import MenuHome from './components/menu'
+import {Layout} from 'antd'
+const {Header, Footer, Sider, Content} = Layout
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      {/* <CustomizedSnackbars /> */}
-      {/* <LayOut> */}
-      <Router />
-      {/* </LayOut> */}
-    </BrowserRouter>
+    <Layout>
+      <Sider>
+        <MenuHome />
+      </Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>
+          <BrowserRouter>
+            {/* <CustomizedSnackbars /> */}
+            {/* <LayOut> */}
+            <Router />
+            {/* </LayOut> */}
+          </BrowserRouter>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
   </Provider>
 )
 
