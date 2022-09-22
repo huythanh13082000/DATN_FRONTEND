d7852a84f493110fbd41880814fe85eb83ca5cf8
luongduy2798 <nhatduy2727@gmail.com>
2022-09-22 18:09:32 +0700

    build code base

---
+++ src/assets/gen/index.tsx
@@ -0,0 +1,7 @@
+/**
+ * WARNING: this file is generated. Don't change it manually.
+ */
+export default class Images {
+    static readonly avatarDefault = require("@src/assets/images/avatar_default.png");
+    static readonly chevronDown = require("@src/assets/images/chevron-down.png");
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import {Provider} from 'react-redux'
-import {store} from './app/store'
-import reportWebVitals from './reportWebVitals'
-import {BrowserRouter} from 'react-router-dom'
-import './index.css'
-import Router from './router'
-import MenuHome from './components/menu'
-import {Layout} from 'antd'
-const {Header, Footer, Sider, Content} = Layout
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Router from "./routers";
+import MenuHome from "./components/menu";
+import { Layout } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
 
-const container = document.getElementById('root')!
-const root = createRoot(container)
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
@@ -30,9 +30,9 @@ root.render(
       </Layout>
     </Layout>
   </Provider>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
