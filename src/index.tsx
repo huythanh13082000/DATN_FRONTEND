b284799171329286c6326cadf6e186e58870f458
ThànhPH <huythanh1308bn@gmail.com>
2022-09-22 23:21:28 +0700

    update 22/09

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import Router from "./routers";
-import MenuHome from "./components/menu";
-import { Layout } from "antd";
-const { Header, Footer, Sider, Content } = Layout;
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import {Provider} from 'react-redux'
+import {store} from './app/store'
+import reportWebVitals from './reportWebVitals'
+import {BrowserRouter} from 'react-router-dom'
+import './index.css'
+import Router from './routers'
+import MenuHome from './components/menu'
+import {Layout} from 'antd'
+const {Header, Footer, Sider, Content} = Layout
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
+const container = document.getElementById('root')!
+const root = createRoot(container)
 
 root.render(
   <Provider store={store}>
-    <Layout>
-      <Sider>
-        <MenuHome />
-      </Sider>
+    {/* <Layout>
+      <Sider> */}
+    <MenuHome />
+    {/* </Sider>
       <Layout>
         <Header>Header</Header>
         <Content>
@@ -28,11 +28,11 @@ root.render(
         </Content>
         <Footer>Footer</Footer>
       </Layout>
-    </Layout>
+    </Layout> */}
   </Provider>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
