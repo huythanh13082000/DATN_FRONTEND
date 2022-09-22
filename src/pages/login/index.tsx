9805c9e0655ca84d7751c2c010308cee94df3c44
HuyThanh <huythanh1308bn@gmail.com>
2022-09-22 16:48:27 +0700

    update 22/09

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import {Col, Row, Input, Button} from 'antd'
+import './login.css'
 
 const Login = () => {
   return (
-    <div>index</div>
+    <>
+      <div className='login-container'>
+        <div className='login-form'>
+          <p className='login-p1'>Login</p>
+          <p className='login-p2'>Sign in to start your session</p>
+          <Row className='login-row'>
+            <Col span={22}>
+              <Input
+                className='login-input'
+                bordered={false}
+                placeholder='Email'
+              />
+            </Col>
+            <Col span={2}>
+              <img src='/img/login-email.svg' alt='' />
+            </Col>
+          </Row>
+          <Row className='login-row'>
+            <Col span={22}>
+              <Input
+                className='login-input'
+                bordered={false}
+                placeholder='Password'
+              />
+            </Col>
+            <Col span={2}>
+              <img src='/img/login-pw.svg' alt='' />
+            </Col>
+          </Row>
+
+          <Button className='login-buuton'>Login</Button>
+        </div>
+      </div>
+
+      {/* <Button type='primary'>Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type='dashed'>Dashed Button</Button>
+      <br />
+      <Button type='text'>Text Button</Button>
+      <Button type='link'>Link Button</Button>
+      <Button></Button> */}
+    </>
   )
 }
 
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,24 @@
-import { Url } from "../contains"
-import Login from "../pages/login"
-import MyRouteProp from "./MyRouteProp"
+import {Url} from '../contains'
+import Home from '../pages/home'
+import Login from '../pages/login'
+import MyRouteProp from './MyRouteProp'
 
 const routes: Array<MyRouteProp> = [
   {
     path: Url.login,
     element: <Login />,
+    private: false,
+    roles: ['user', 'admin', 'super'],
+  },
+  {
+    path: Url.home,
+    element: <Home />,
+    private: false,
+    roles: ['user', 'admin', 'super'],
+  },
+  {
+    path: '/',
+    element: <Home />,
     private: false,
     roles: ['user', 'admin', 'super'],
   },
