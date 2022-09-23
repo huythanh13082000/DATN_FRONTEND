d1deec270bf794f2b8eb098f2b4eedc4020f19a9
HuyThanh <huythanh1308bn@gmail.com>
2022-09-23 18:03:27 +0700

    update header

--- src/assets/gen/index.tsx
+++ src/assets/gen/index.tsx
@@ -7,6 +7,10 @@ export default class Images {
     static readonly gift = require("../../../src/assets/images/gift.png");
     static readonly headerIcon = require("../../../src/assets/images/header-icon.png");
     static readonly headerIcon1 = require("../../../src/assets/images/header-icon1.png");
+    static readonly helpImg1 = require("../../../src/assets/images/help-img1.png");
+    static readonly helpImg2 = require("../../../src/assets/images/help-img2.png");
+    static readonly helpImg3 = require("../../../src/assets/images/help-img3.png");
+    static readonly helpImg4 = require("../../../src/assets/images/help-img4.png");
     static readonly icAdd = require("../../../src/assets/images/ic-add.png");
     static readonly searchNormal = require("../../../src/assets/images/search-normal.png");
     static readonly sms = require("../../../src/assets/images/sms.png");
--- src/components/cardHelp/index.tsx
+++ src/components/cardHelp/index.tsx
@@ -1,18 +1,21 @@
 import {Col, Row} from 'antd'
 import React from 'react'
+import Images from '../../assets/gen'
 import './cardHelp.css'
 
 const CardHelp = () => {
   return (
     <div className='ch_container'>
       <p>Help Center</p>
-      <Row>
-        <Col span={6}>
-          <img src='' alt='' />
+      <Row className='ch-row1'>
+        <Col span={4}>
+          <img src={Images.helpImg1} alt='' />
         </Col>
         <Col>
-          <p>Knowledge Base</p>
-          <p>Comprehensive guide on using the joyride platform</p>
+          <p className='ch_p1'>Knowledge Base</p>
+          <p className='ch_p2'>
+            Comprehensive guide on using the joyride platform
+          </p>
         </Col>
       </Row>
     </div>
