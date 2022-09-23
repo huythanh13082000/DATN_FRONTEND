7fa195ebd8f160fb2f73b0cbd8a6c17b972207c2
HuyThanh <huythanh1308bn@gmail.com>
2022-09-23 14:42:36 +0700

    update translate menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,12 +1,3 @@
-import {
-  AppstoreOutlined,
-  ContainerOutlined,
-  DesktopOutlined,
-  MailOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
-} from '@ant-design/icons'
 import type {MenuProps} from 'antd'
 import {Button, Menu} from 'antd'
 import React, {useState} from 'react'
@@ -35,52 +26,64 @@ const MenuHome: React.FC = () => {
   const {t} = useTranslation()
 
   const items: MenuItem[] = [
-    getItem(t('Analytics'), '1', <i className='fa-solid fa-house'></i>),
-    getItem('Orgs', 'sub1', <i className='fa-solid fa-people-group'></i>, [
-      getItem('Org', '8', <i className='fa-solid fa-sitemap'></i>),
-      getItem('Role List', '9', <i className='fa-solid fa-circle-user'></i>),
-      getItem('Permissions', '10', <i className='fa-solid fa-key'></i>),
+    getItem(t('Menu.1'), '1', <i className='fa-solid fa-house'></i>),
+    getItem(t('Menu.2'), 'sub1', <i className='fa-solid fa-people-group'></i>, [
+      getItem(t('Menu.2_1'), '8', <i className='fa-solid fa-sitemap'></i>),
+      getItem(t('Menu.2_2'), '9', <i className='fa-solid fa-circle-user'></i>),
+      getItem(t('Menu.2_3'), '10', <i className='fa-solid fa-key'></i>),
     ]),
-    getItem('Maps', '2', <i className='fa-solid fa-location-dot'></i>),
-    getItem('Fleets', 'sub2', <i className='fa-solid fa-map-location-dot'></i>),
-    getItem('Vehicles', 'sub3', <i className='fa-solid fa-motorcycle'></i>, [
-      getItem('All', '11'),
-      getItem('Reserved', '12'),
-      getItem('Vehicle History', '13'),
-    ]),
-    getItem('Geofence', '3', <i className='fa-brands fa-product-hunt'></i>, [
-      getItem('Parking Zones', '14'),
-      getItem('Restricted Zones', '15'),
-      getItem('Service Areas', '16'),
-      getItem('Slow Speed Zones', '17'),
-      getItem('Stations', '18'),
-    ]),
-    getItem('Riders', '19', <i className='fa-solid fa-person-biking'></i>),
-    getItem('Rides', 'sub4', <i className='fa-solid fa-person-biking'></i>),
-    getItem('Payments', '4', <i className='fa-solid fa-credit-card'></i>),
+    getItem(t('Menu.3'), '2', <i className='fa-solid fa-location-dot'></i>),
     getItem(
-      'Pricing',
+      t('Menu.4'),
+      'sub2',
+      <i className='fa-solid fa-map-location-dot'></i>
+    ),
+    getItem(t('Menu.5'), 'sub3', <i className='fa-solid fa-motorcycle'></i>, [
+      getItem(t('Menu.5_1'), '11'),
+      getItem(t('Menu.5_2'), '12'),
+      getItem(t('Menu.5_3'), '13'),
+    ]),
+    getItem(t('Menu.6'), '3', <i className='fa-brands fa-product-hunt'></i>, [
+      getItem(t('Menu.6_1'), '14'),
+      getItem(t('Menu.6_2'), '15'),
+      getItem(t('Menu.6_3'), '16'),
+      getItem(t('Menu.6_4'), '17'),
+      getItem(t('Menu.6_5'), '18'),
+    ]),
+    getItem(t('Menu.7'), '19', <i className='fa-solid fa-person-biking'></i>),
+    getItem(
+      t('Menu.8'),
+      'sub4',
+      <i className='fa-solid fa-person-biking'></i>,
+      [
+        getItem(t('Menu.8_1'), '29'),
+        getItem(t('Menu.8_2'), '30'),
+      ]
+    ),
+    getItem(t('Menu.9'), '4', <i className='fa-solid fa-credit-card'></i>),
+    getItem(
+      t('Menu.10'),
       'sub5',
       <i className='fa-solid fa-circle-dollar-to-slot'></i>,
       [
-        getItem('Payment Gateway', '20'),
-        getItem('Billing Plan', '21'),
-        getItem('Tax', '22'),
-        getItem('Promos', '23'),
-        getItem('Offer', '24'),
+        getItem( t('Menu.10_1'), '20'),
+        getItem( t('Menu.10_2'), '21'),
+        getItem( t('Menu.10_3'), '22'),
+        getItem( t('Menu.10_4'), '23'),
+        getItem( t('Menu.10_5'), '24'),
       ]
     ),
-    getItem('Support', 'sub6', <i className='fa-solid fa-headset'></i>, [
-      getItem('Tickets', '25'),
-      getItem('Feedback Option', '26'),
+    getItem(t('Menu.11'), 'sub6', <i className='fa-solid fa-headset'></i>, [
+      getItem(t('Menu.11_1'), '25'),
+      getItem(t('Menu.11_2'), '26'),
     ]),
-    getItem('Campaigns', 'sub7', <i className='fa-solid fa-bullhorn'></i>, [
-      getItem('Notifications', '27'),
-      getItem('Banner', '28'),
+    getItem(t('Menu.12'), 'sub7', <i className='fa-solid fa-bullhorn'></i>, [
+      getItem(t('Menu.12_1'), '27'),
+      getItem(t('Menu.12_2'), '28'),
     ]),
-    getItem('Application Theme', '5', <i className='fa-solid fa-palette'></i>),
-    getItem('Events', '6', <i className='fa-solid fa-calendar'></i>),
-    getItem('Settings', '7', <i className='fa-solid fa-gear'></i>),
+    getItem(t('Menu.13'), '5', <i className='fa-solid fa-palette'></i>),
+    getItem(t('Menu.14'), '6', <i className='fa-solid fa-calendar'></i>),
+    getItem(t('Menu.15'), '7', <i className='fa-solid fa-gear'></i>),
   ]
 
   return (
@@ -89,9 +92,7 @@ const MenuHome: React.FC = () => {
       defaultSelectedKeys={['1']}
       mode='inline'
       theme='light'
-      // inlineCollapsed={collapsed}
       items={items}
-      // title='logo'
     />
   )
 }
