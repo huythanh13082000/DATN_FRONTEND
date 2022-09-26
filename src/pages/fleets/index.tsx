2b92058fafd84bf4ac9054dba440ba97e7da53b4
HuyThanh <huythanh1308bn@gmail.com>
2022-09-26 18:01:53 +0700

    update 26/09

--- src/pages/fleets/index.tsx
+++ src/pages/fleets/index.tsx
@@ -1,12 +1,24 @@
-import {Col, Row} from 'antd'
+import {Col, Row, Select} from 'antd'
+import {SizeType} from 'antd/lib/config-provider/SizeContext'
+import {Option} from 'antd/lib/mentions'
 import React, {useState} from 'react'
 import BBtn from '../../components/button'
+import ISearch from '../../components/search'
 import ITable from '../../components/table'
 import InputGroup from '../../components/textInput'
 
+const children: React.ReactNode[] = []
+for (let i = 10; i < 36; i++) {
+  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>)
+}
+
 const Fleets = () => {
-  const [dataRole, setDataRole] = useState<any>()
+  const [dataFleet, setDataFleet] = useState<any>(true)
   const [openAdd, setOpenAdd] = useState(false)
+  const [size, setSize] = useState<SizeType>('middle')
+  const handleChange = (value: string | string[]) => {
+    console.log(`Selected: ${value}`)
+  }
   return (
     <div>
       <br />
@@ -51,8 +63,28 @@ const Fleets = () => {
           <>
             <Col span={24} className='gutter-row'>
               <div className='org_div1'>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <span style={{marginRight: '1rem'}}>Show</span>
+                  <Select
+                    size={size}
+                    defaultValue={'Show'}
+                    onChange={handleChange}
+                  >
+                    {children}
+                  </Select>
+                  <span style={{marginLeft: '1rem', marginRight: '3rem'}}>
+                    Entries
+                  </span>
+                  <ISearch onChange={() => console.log(222)} />
+                </div>
+                <br />
                 <ITable
-                  onClickRow={(params: any) => setDataRole(params)}
+                  onClickRow={(params: any) => setDataFleet(params)}
                   column={[
                     {
                       title: 'ID',
@@ -139,35 +171,96 @@ const Fleets = () => {
               </div>
             </Col>
 
-            {dataRole && (
-              <Col span={12} className='gutter-row'>
-                <div className='org_div1'>
-                  <p className='org_p1'>Edit Role</p>
-                  <Row gutter={16}>
-                    <Col span={12}>
-                      <InputGroup
-                        required={true}
-                        title='Role Name'
-                        name='roleName'
-                        onChange={() => console.log(111)}
-                      />
-                    </Col>
-                  </Row>
+            {dataFleet && (
+              <>
+                <Col span={24} className='gutter-row'>
                   <br />
-                  <Row style={{borderTop: '1px solid #E1E1E1'}}></Row>
                   <br />
-                  <div
-                    style={{display: 'flex', justifyContent: 'space-between'}}
-                  >
-                    <BBtn
-                      title='Back'
-                      color='#FF5C6A'
-                      onClick={() => setDataRole(undefined)}
-                    />
-                    <BBtn title='Save Changes' color='#24BB69' />
+                  <div style={{display: 'flex'}}>
+                    <BBtn title='Edit' color='#2C405A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='General' color='#FF5C6A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='Company' color='#FF5C6A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='Mail Configuration' color='#FF5C6A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='Vehicle Configuration' color='#FF5C6A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='Timeline' color='#FF5C6A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='Operation' color='#FF5C6A' />
+                    <span style={{width: '0.5rem'}}></span>
+                    <BBtn title='Pre Ride Screen' color='#FF5C6A' />
                   </div>
-                </div>
-              </Col>
+                </Col>
+                <Col span={24} className='gutter-row'>
+                  <div className='org_div1'>
+                    <Row gutter={16}>
+                      <Col span={12}>
+                        <InputGroup
+                          required={true}
+                          title='Name'
+                          name='name'
+                          onChange={() => console.log(111)}
+                        />
+                      </Col>
+                      <Col span={12}>
+                        <InputGroup
+                          title='Country'
+                          name='country'
+                          onChange={() => console.log(111)}
+                        />
+                      </Col>
+                      <Col span={12}>
+                        <InputGroup
+                          required={true}
+                          title='Alert Email'
+                          name='alertEmail'
+                          onChange={() => console.log(111)}
+                        />
+                      </Col>
+                      <Col span={12}>
+                        <InputGroup
+                          required={true}
+                          title='Visiblity'
+                          name='visiblity'
+                          onChange={() => console.log(111)}
+                        />
+                      </Col>
+                      <Col span={12}>
+                        <InputGroup
+                          disabled
+                          required={true}
+                          title='Latitude'
+                          name='latitude'
+                          onChange={() => console.log(111)}
+                        />
+                      </Col>
+                      <Col span={12}>
+                        <InputGroup
+                          disabled
+                          required={true}
+                          title='Longiude'
+                          name='longiude'
+                          onChange={() => console.log(111)}
+                        />
+                      </Col>
+                    </Row>
+                    <br />
+                    <Row style={{borderTop: '1px solid #E1E1E1'}}></Row>
+                    <br />
+                    <div style={{display: 'flex', justifyContent: 'end'}}>
+                      <BBtn
+                        title='Delete'
+                        color='#FF5C6A'
+                        onClick={() => setOpenAdd(false)}
+                      />
+                      <BBtn title='Save Changes' color='#24BB69' />
+                    </div>
+                  </div>
+                </Col>
+              </>
             )}
           </>
         )}
