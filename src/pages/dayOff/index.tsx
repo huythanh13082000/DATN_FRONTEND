f813ae074497fb4bcd2c363f06396da39099abf2
ThànhPH <huythanh1308bn@gmail.com>
2022-12-19 23:41:40 +0700

    update 19/12

--- src/pages/dayOff/index.tsx
+++ src/pages/dayOff/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import {Col, DatePicker, Input, Row} from 'antd'
+import moment from 'moment'
+import React, {useState} from 'react'
 import TableCustom from '../../components/tableCustom'
-import { Url } from '../../routers/paths'
-import { personnelDayOffColumn } from '../../utils/columnTable'
+import {Url} from '../../routers/paths'
+import {personnelDayOffColumn} from '../../utils/columnTable'
 
 const PersonnelDayOff = () => {
+  const [filter, setFilter] = useState<any>()
   return (
-    <div><TableCustom url={Url.personnelDayOff} columns={personnelDayOffColumn} disableExportExcel/></div>
+    <div>
+      <Row gutter={16}>
+        <Col span={4}>
+          <p>Tìm kiếm theo tên: </p>
+          <Input
+            placeholder='Nhập tên'
+            onChange={(e) => setFilter({...filter, name: e.target.value})}
+          />
+        </Col>
+        <Col span={4}>
+          <p>Tìm kiếm theo ngày nghỉ: </p>
+          <DatePicker
+            onChange={(e) => {
+              setFilter({...filter, dayOff: e?.format()})
+            }}
+            style={{width: '100%'}}
+          />
+        </Col>
+      </Row>
+      <TableCustom
+        url={Url.personnelDayOff}
+        columns={personnelDayOffColumn}
+        disableExportExcel
+        filter={filter}
+      />
+    </div>
   )
 }
 
--- src/pages/department/index.tsx
+++ src/pages/department/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import {Col, Input, Row} from 'antd'
+import React, {useState} from 'react'
 import {urlApi} from '../../apis/url'
 import TableCustom from '../../components/tableCustom'
-import { columnsDepartment } from '../../utils/columnTable'
+import {columnsDepartment} from '../../utils/columnTable'
 
 const Department = () => {
+  const [filter, setFilter] = useState<any>()
   return (
     <div>
+      <Row gutter={16}>
+        <Col span={4}>
+          <p>Tìm kiếm theo tên: </p>
+          <Input
+            placeholder='Nhập tên'
+            onChange={(e) => setFilter({...filter, name: e.target.value})}
+          />
+        </Col>
+      </Row>
       <TableCustom
         url={urlApi.department}
         columns={columnsDepartment}
-        searchName
+        filter={filter}
         disableExportExcel
       />
     </div>
